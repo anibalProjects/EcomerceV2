4fa923be45496cd181d07e65119f0b1727acb504
anibalProjects <anibal.projects@gmail.com>
2026-05-13 10:11:03 +0200

    Arreglo de imagenes y filtrado

--- forniture_api/database/seeders/ImageSeeder.php
+++ forniture_api/database/seeders/ImageSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Image;
 use App\Models\Mueble;
+use Illuminate\Support\Facades\DB;
 
 class ImageSeeder extends Seeder
 {
@@ -14,28 +14,34 @@ class ImageSeeder extends Seeder
      */
     public function run(): void
     {
-        // URLs de ejemplo de Unsplash para muebles
-        $urls = [
-            'https://images.unsplash.com/photo-1555041469-a586c61ea9bc', // Sofá
-            'https://images.unsplash.com/photo-1505691938895-1758d7feb511', // Dormitorio
-            'https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd', // Oficina
-            'https://images.unsplash.com/photo-1556912173-3bb406ef7e77', // Cocina
-            'https://images.unsplash.com/photo-1524758631624-e2822e304c36', // Oficina Pro
-            'https://images.unsplash.com/photo-1594026112284-02bb6f3352fe', // Mesa
+        DB::table('mueble_image')->delete();
+        Image::query()->delete();
+
+        $mueblesImages = [
+            'Sofá Minimalista' => 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?auto=format&fit=crop&w=800&q=80',
+            'Cama King Size' => 'https://images.unsplash.com/photo-1505693314120-0d443867891c?auto=format&fit=crop&w=800&q=80',
+            'Escritorio Elevable' => 'https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd?auto=format&fit=crop&w=800&q=80',
+            'Mesa de Comedor' => 'https://images.unsplash.com/photo-1580480055273-228ff5388ef8?auto=format&fit=crop&w=800&q=80',
+            'Silla de Oficina Pro' => 'https://images.unsplash.com/photo-1505843490538-5133c6c7d0e1?auto=format&fit=crop&w=800&q=80',
+            'Estantería Industrial' => 'https://images.unsplash.com/photo-1594620302200-9a762244a156?auto=format&fit=crop&w=800&q=80',
+            'Mueble de Cocina Modular' => 'https://images.unsplash.com/photo-1556912173-3bb406ef7e77?auto=format&fit=crop&w=800&q=80',
+            'Armario de Dormitorio' => 'https://images.unsplash.com/photo-1595526114035-0d45ed16cfbf?auto=format&fit=crop&w=800&q=80',
+            'Butaca de Terciopelo' => 'https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?auto=format&fit=crop&w=800&q=80',
+            'Mesita de Noche Vintage' => 'https://images.unsplash.com/photo-1532372320572-cda25653a26d?auto=format&fit=crop&w=800&q=80',
+            'Aparador Nórdico' => 'https://images.unsplash.com/photo-1594026112284-02bb6f3352fe?auto=format&fit=crop&w=800&q=80',
+            'Silla de Comedor Velvet' => 'https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?auto=format&fit=crop&w=800&q=80',
+            'Escritorio Juvenil' => 'https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd?auto=format&fit=crop&w=800&q=80',
+            'Isla de Cocina Portátil' => 'https://images.unsplash.com/photo-1556909114-f6e7ad7d3136?auto=format&fit=crop&w=800&q=80',
+            'Espejo de Cuerpo Entero' => 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?auto=format&fit=crop&w=800&q=80',
         ];
 
-        foreach ($urls as $url) {
-            Image::create(['url' => $url]);
-        }
-
-        // Asociar imágenes a muebles de forma aleatoria
         $muebles = Mueble::all();
-        $images = Image::all();
 
         foreach ($muebles as $mueble) {
-            // Cada mueble tendrá entre 1 y 2 imágenes aleatorias
-            $randomImages = $images->random(rand(1, 2))->pluck('id');
-            $mueble->galeria()->attach($randomImages);
+            if (isset($mueblesImages[$mueble->nombre])) {
+                $image = Image::create(['url' => $mueblesImages[$mueble->nombre]]);
+                $mueble->galeria()->attach($image->id);
+            }
         }
     }
 }
